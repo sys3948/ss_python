c18145bdb6daa947276bc54f0bedd5717145de7d
limsoo5788 <limsoo5788@gmail.com>
2020-11-17 12:10:06 +0900

    SQLAlchemy 로 Task 테이블 작성, User 테이블에 tasks 관계 생성

--- app/models.py
+++ app/models.py
@@ -7,4 +7,17 @@ class User(db.Model):
     password_hash = db.Column(db.String(64))
     username = db.Column(db.String(32), unique=True, index=True)
     phone_number = db.Column(db.String(20))
-    confirm = db.Column(db.Boolean)
+    confirm = db.Column(db.Boolean, default=False)
+
+    tasks = db.relationship('Task', backref = 'author', lazy='dynamic')
+
+class Task(db.Model):
+    task_id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id', primary_key = True)
+    title = db.Column(db.String(128))
+    start_date = db.Column(db.DateTime())
+    end_date = db.Column(db.DateTime())
+    comment_path = db.Column(db.String(256))
+    review_path = db.Column(db.String(256))
+    log_path = db.Column(db.String(256))
+    checked = db.Column(db.Boolean, default=False)
