b23354cd37fff5a0b3d6ca4d7b172a17748b1d13
yun <sys394880@gmail.com>
2020-11-19 10:06:19 +0900

    정의한 model들 migration 적용!

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from . import db
 
 
 class User(db.Model):
-    __tablename__ = 'user';
+    __tablename__ = 'user'
     user_id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(64), nullable = False)
@@ -15,9 +15,9 @@ class User(db.Model):
 
 
 class Task(db.Model):
-    __tablename__ = 'task';
+    __tablename__ = 'task'
     task_id = db.Column(db.Integer, primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key = True)
     title = db.Column(db.String(128), nullable=False)
     start_date = db.Column(db.DateTime())
     end_date = db.Column(db.DateTime())
@@ -32,26 +32,26 @@ class Task(db.Model):
 
 
 class Task_sharing(db.Model):
-    __tablename__ = 'task_sharing';
-    task_id = db.Column(db.Integer, db.ForeignKey('Task.task_id'), primary_key=True)
-    admin_id = db.Column(db.Integer, db.ForeignKey('Task.user_id'), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), primary_key=True)
+    __tablename__ = 'task_sharing'
+    task_id = db.Column(db.Integer, db.ForeignKey('task.task_id'), primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey('task.user_id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
     level = db.Column(db.Integer)
 
 
 class Contents(db.Model):
-    __tablename__ = 'contents';
+    __tablename__ = 'contents'
     contents_id = db.Column(db.Integer, primary_key = True)
-    task_id = db.Column(db.Integer, db.ForeignKey('Task.task_id'), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Task.user_id'), primary_key=True)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.task_id'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('task.user_id'), primary_key=True)
     name = db.Column(db.String(128), index=True, nullable=False)
     level = db.Column(db.Integer)
 
 
 class Alarm(db.Model):
     __tablename__ = 'alarm'
-    task_id = db.Column(db.Integer, db.ForeignKey('Task.task_id', primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Task.user_id', primary_key = True)
+    task_id = db.Column(db.Integer, db.ForeignKey('task.task_id'), primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('task.user_id'), primary_key = True)
     email = db.Column(db.Boolean, default = False)
     kakao = db.Column(db.Boolean, default = False)
     line = db.Column(db.Boolean, default = False)
@@ -67,7 +67,7 @@ class Alarm(db.Model):
 class Alarm_time(db.Model):
     __tablename__ = 'alarm_time'
     alarm_id = db.Column(db.Integer, primary_key = True)
-    task_id = db.Column(db.Integer, db.ForeignKey('Alarm.task_id', primary_key = True)
-    user_id = db.Column(db.Integer, db.ForeignKey('Alarm.user_id', primary_key = True)
+    task_id = db.Column(db.Integer, db.ForeignKey('alarm.task_id'), primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('alarm.user_id'), primary_key = True)
     time = db.Column(db.DateTime())
 
