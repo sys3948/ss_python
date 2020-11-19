a2440d1dd19f0be34477928ed99a169dc306bc2b
limsoo5788 <limsoo5788@gmail.com>
2020-11-19 11:46:27 +0900

    개인컴퓨터 migrate 연습 후 결과물

--- runserver.py
+++ runserver.py
@@ -3,7 +3,6 @@ from flask_script import Manager, Shell
 from flask_migrate import Migrate
 from app.models import User, Task, Task_sharing, Contents, Alarm, Alarm_time
 
-
 app = create_app('default')
 manager = Manager(app)
 migrate = Migrate(app, db)
@@ -11,7 +10,6 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(app = app, db = db, User = User, Task = Task, Task_sharing = Task_sharing, Contents = Contents, Alarm = Alarm, Alarm_time = Alarm_time)
-
 
 # if __name__ == "__main__":
 #     manager.run(debug=True)
