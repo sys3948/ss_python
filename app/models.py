ffbcf553388a0b537cd6662f886e28cea2b8ee43
yun <sys394880@gmail.com>
2020-11-19 09:20:50 +0900

    병합 충돌 후 models.py 정리

--- app/models.py
+++ app/models.py
@@ -5,9 +5,9 @@ class User(db.Model):
     __tablename__ = 'user';
     user_id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(64))
-    username = db.Column(db.String(32), unique=True, index=True)
-    phone_number = db.Column(db.String(20))
+    password_hash = db.Column(db.String(64), nullable = False)
+    username = db.Column(db.String(32), nullable = False, unique=True, index=True)
+    phone_number = db.Column(db.String(20), nullable = False)
     confirm = db.Column(db.Boolean, default=False)
 
     tasks = db.relationship('Task', backref = 'author', lazy='dynamic')
@@ -18,7 +18,7 @@ class Task(db.Model):
     __tablename__ = 'task';
     task_id = db.Column(db.Integer, primary_key = True)
     user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'), primary_key = True)
-    title = db.Column(db.String(128))
+    title = db.Column(db.String(128), nullable=False)
     start_date = db.Column(db.DateTime())
     end_date = db.Column(db.DateTime())
     comment_path = db.Column(db.String(256))
@@ -28,6 +28,7 @@ class Task(db.Model):
 
 
     tasks_sharing = db.relationship('Task_sharing', backref = 'task', lazy = 'dynamic')
+    alarms = db.relationship('Alarm', backref = 'todo', lazy='dynamic')
 
 
 class Task_sharing(db.Model):
@@ -47,9 +48,6 @@ class Contents(db.Model):
     level = db.Column(db.Integer)
 
 
-    alarms = db.relationship('User', backref = 'todo', lazy='dynamic')
-
-
 class Alarm(db.Model):
     __tablename__ = 'alarm'
     task_id = db.Column(db.Integer, db.ForeignKey('Task.task_id', primary_key = True)
@@ -63,7 +61,8 @@ class Alarm(db.Model):
     sms = db.Column(db.Boolean, default = False)
     default = db.Column(db.Boolean, default = False)
 
-    alarm_times = db.relationship('User', backref = 'todo_alarm', lazy='dynamic')
+    alarm_times = db.relationship('Alarm_time', backref = 'todo_alarm', lazy='dynamic')
+
 
 class Alarm_time(db.Model):
     __tablename__ = 'alarm_time'
