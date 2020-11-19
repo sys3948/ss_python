2bce86850231e39ddcb17624b8f673a0449c0ff1
limsoo5788 <limsoo5788@gmail.com>
2020-11-19 09:05:53 +0900

    alarm과 alarm_time 모델링 작성

--- app/models.py
+++ app/models.py
@@ -21,3 +21,25 @@ class Task(db.Model):
     review_path = db.Column(db.String(256))
     log_path = db.Column(db.String(256))
     checked = db.Column(db.Boolean, default=False)
+
+    alarms = db.relationship('User', backref = 'todo', lazy='dynamic')
+
+class Alarm(db.Model):
+    task_id = db.Column(db.Integer, db.ForeignKey('Task.task_id', primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('Task.user_id', primary_key = True)
+    email = db.Column(db.Boolean, default = False)
+    kakao = db.Column(db.Boolean, default = False)
+    line = db.Column(db.Boolean, default = False)
+    f_message = db.Column(db.Boolean, default = False)
+    instar_dm = db.Column(db.Boolean, default = False)
+    twitter_dm = db.Column(db.Boolean, default = False)
+    sms = db.Column(db.Boolean, default = False)
+    default = db.Column(db.Boolean, default = False)
+
+    alarm_times = db.relationship('User', backref = 'todo_alarm', lazy='dynamic')
+
+class Alarm_time(db.Model):
+    alarm_id = db.Column(db.Integer, primary_key = True)
+    task_id = db.Column(db.Integer, db.ForeignKey('Alarm.task_id', primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('Alarm.user_id', primary_key = True)
+    time = db.Column(db.DateTime())
